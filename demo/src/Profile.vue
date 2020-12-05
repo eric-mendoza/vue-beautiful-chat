8355c6dface9454410ab567ac19e28b37fff5a9f
eric-mendoza <eric.mendoza333@gmail.com>
2020-12-04 18:15:03 -0600

    v2.6.12

---
+++ demo/src/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile--container">
+    Profile data...
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Profile",
+    props: {
+      profile: {
+        required: true,
+        type: Object,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .profile--container{
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    overflow-x: hidden;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+  }
+
+</style>
